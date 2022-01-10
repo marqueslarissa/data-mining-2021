4b4234fd3b3c7dfd0c618e91f4036f4a9930f52a
Larissa Marques <lmarques@acesso.com>
2022-01-10 05:21:19 -0300

    remove colunas extras do dataset final

--- train_test.py
+++ train_test.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 df = pd.read_csv('cleaning_dataset/pt4/Detail_Incident_Activity_cut6_pt4.csv', sep=',', low_memory=False)
 
+df.drop(columns=["Unnamed: 0", "r"],inplace=True)
+
 splitter = GroupShuffleSplit(test_size=.20, n_splits=2, random_state = 7)
 split = splitter.split(df, groups=df['incident_id'])
 train_inds, test_inds = next(split)
