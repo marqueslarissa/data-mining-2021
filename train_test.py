59b3cfc5bf0227aaed7e05825e407897dca24f23
Larissa Marques <lmarques@acesso.com>
2022-01-12 10:24:42 -0300

    altera corte para atividades com media de resolucao 0

--- train_test.py
+++ train_test.py
@@ -5,7 +5,14 @@ df = pd.read_csv('cleaning_dataset/pt4/Detail_Incident_Activity_cut6_pt4.csv', s
 
 df.drop(columns=["Unnamed: 0", "r"],inplace=True)
 
-splitter = GroupShuffleSplit(test_size=.20, n_splits=2, random_state = 7)
+df['sojourn_set_stc'] = 0
+df['sojourn_mset_stc'] = 0
+df['sojourn_seq_stc'] = 0
+df['seq_state_id'] = 0
+df['set_state_id'] = 0
+df['mset_state_id'] = 0
+
+splitter = GroupShuffleSplit(test_size=.40, n_splits=2, random_state = 7)
 split = splitter.split(df, groups=df['incident_id'])
 train_inds, test_inds = next(split)
 
@@ -17,18 +24,18 @@ df_ATS = df_ATS.rename(columns={'incident_id': 'number', 'datestamp' : 'updated_
 train = df_ATS.iloc[train_inds]
 test = df_ATS.iloc[test_inds]
 
-train.to_csv('cleaning_dataset/train_test/ATS_1_TRAIN_PREPROCESSED_Detail_Incident_Activity_cut6.csv', sep=',', encoding='utf-8', index=False)
-test.to_csv('cleaning_dataset/train_test/ATS_2_TEST_PREPROCESSED_Detail_Incident_Activity_cut6.csv', sep=',', encoding='utf-8', index=False)
+train.to_csv('cleaning_dataset/train_test/ATS_1_6040_TRAIN_PREPROCESSED_Detail_Incident_Activity_cut6.csv', sep=',', encoding='utf-8', index=False)
+test.to_csv('cleaning_dataset/train_test/ATS_1_6040_TEST_PREPROCESSED_Detail_Incident_Activity_cut6.csv', sep=',', encoding='utf-8', index=False)
 
 #rename column for LSTM
-df_LSTM = df
-df_LSTM = df_LSTM.rename(columns={'incident_id': 'CaseID', 'datestamp' : 'CompleteTimestamp', 'incidentactivity_type' : 'Activity'})
-df_LSTM.Activity = pd.Categorical(df_LSTM.Activity)
-df_LSTM['ActivityID'] = df_LSTM.Activity.cat.codes
-df_LSTM = df_LSTM[['CaseID', 'ActivityID', 'CompleteTimestamp']]
-
-train = df_LSTM.iloc[train_inds]
-test = df_LSTM.iloc[test_inds]
-
-train.to_csv('cleaning_dataset/train_test/LSTM_1_TRAIN_PREPROCESSED_Detail_Incident_Activity_cut6.csv', sep=',', encoding='utf-8', index=False)
-test.to_csv('cleaning_dataset/train_test/LSTM_2_TEST_PREPROCESSED_Detail_Incident_Activity_cut6.csv', sep=',', encoding='utf-8', index=False)
+# df_LSTM = df
+# df_LSTM = df_LSTM.rename(columns={'incident_id': 'CaseID', 'datestamp' : 'CompleteTimestamp', 'incidentactivity_type' : 'Activity'})
+# df_LSTM.Activity = pd.Categorical(df_LSTM.Activity)
+# df_LSTM['ActivityID'] = df_LSTM.Activity.cat.codes
+# df_LSTM = df_LSTM[['CaseID', 'ActivityID', 'CompleteTimestamp']]
+# 
+# train = df_LSTM.iloc[train_inds]
+# test = df_LSTM.iloc[test_inds]
+# 
+# train.to_csv('cleaning_dataset/train_test/LSTM_1_TRAIN_PREPROCESSED_Detail_Incident_Activity_cut6.csv', sep=',', encoding='utf-8', index=False)
+# test.to_csv('cleaning_dataset/train_test/LSTM_1_TEST_PREPROCESSED_Detail_Incident_Activity_cut6.csv', sep=',', encoding='utf-8', index=False)
